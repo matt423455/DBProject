42fecac4bd5d48ab56900886fd10c9d8091c786a
Matthew Idoni <matthew.idoni@numonix.io>
2025-03-22 18:23:37 -0400

    test

--- html/API/config.php
+++ html/API/config.php
@@ -3,8 +3,8 @@ session_start();
 
 $DB_HOST = 'localhost';         // or '127.0.0.1'
 $DB_NAME = 'college_events';    // the DB you created or already have
-$DB_USER = 'myuser';            // the user you created or found
-$DB_PASS = 'mySecretPass!';     // the password for that user
+$DB_USER = 'root';            // the user you created or found
+$DB_PASS = '';     // the password for that user
 
 try {
     $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS);
